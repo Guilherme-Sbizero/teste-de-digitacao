dc24da4e470b76dc30e750fdcd4b0bb83e24cf19
Gulherme-Sbizero <guilhermesbizero@outlook.com>
2023-09-01 12:00:49 -0300

    colocando textos para digitacao

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
-const text = document.querySelector("#texto");
-const text = document.querySelector("#texto");
-const text = document.querySelector("#texto");
-const text = document.querySelector("#texto");
-const text = document.querySelector("#texto");
-const text = document.querySelector("#texto");
+const texto = document.querySelector("#texto");
+const entrada = document.querySelector("#entrada");
+const reiniciar = document.querySelector("#reiniciar");
+const resultado = document.querySelector("#resultado");
+const historico = document.querySelector("#historico");
+const alterarTemaBtn = document.querySelector("#alterarTema");
+
+const textos = [
+    "Ser ou não ser, eis a questão",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    "Esse, saepe veniam.",
+    "Dolorum laudantium voluptatem commodi voluptatum voluptate distinctio praesentium quod.",
+    " Deserunt dolor illo eligendi totam dolores amet consequuntur fugiat deleniti."
+];
